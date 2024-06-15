a9c7df7b579018a864bdfb40825588c126fcbf91
Udhaykrishn <krishnauday320@gmail.com>
2024-06-15 19:30:11 +0530

    added clerk webhook

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal:true,
+    envFilePath:".env"
+  }),UserModule, PrismaModule],
   controllers: [AppController],
 })
 export class AppModule {}
